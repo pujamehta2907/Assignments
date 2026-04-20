7cf6621a15f37684fc64f80b84993255acfd45f2
puja mehta <pujamehta1200@gmail.com>
2026-04-20 14:54:52 +0530

    Assignment 1

---
+++ javascript-training/language-basics/operators.js
@@ -0,0 +1,23 @@
+/* let a= 10;
+let b= 20;
+console.log("post Increament:"+(a++));
+console.log("next line to post Increament: "+(a));
+
+console.log("pre Increament:"+(++b));
+console.log("Next line to pre Increament : " +(b));
+
+console.log("post decreament:" +(a--));
+console.log("next line of post decreament:"+(a));
+
+console.log("pre decreament:"+(--b));
+console.log("next line of pre decreament:"+(b));
+ */
+/* let i=10;
+let j=20;
+console.log((i<j) && (i==j));
+console.log((i<j) || (i==j));
+console.log(!(i<j) && (i==j));
+ */
+let age= 37;
+let result = (age<=18) ? "Elgble to vote" : "not eligble to vote";
+console.log(result); 
---
+++ javascript-training/language-basics/variable-declaration.js
@@ -0,0 +1,19 @@
+/* var a;
+let b;
+const c=10;
+ 
+
+a= 5;
+b= 9; */
+//c=90;
+
+{
+var x= 100;
+let y= 200;
+const z=300;
+
+
+}
+   console.log(x);
+//console.log(y);
+//console.log(z);
